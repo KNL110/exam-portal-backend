93a863644aa3cee16016edc2b7321da30a47536a
KNL110 <asarikrunal9@gmail.com>
2025-07-12 04:22:51 +0530

    setup express app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin : process.env.CROS_ORIGIN,
+    credentials : true
+}));
+
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended:true, limit:"16kb"}));
+
+
+export {app};

acb5798ff0c8b0cb4f77e88e561379c198e7bef2
KNL110 <asarikrunal9@gmail.com>
2025-07-12 16:13:35 +0530

    add ApiError

---
+++ src/utils/ApiError.js
@@ -0,0 +1,11 @@
+class ApiError extends Error {
+    constructor(statusCode, message = "Something went wrong", data = null) {
+        super(message);
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = false;
+    }
+}
+
+export { ApiError };

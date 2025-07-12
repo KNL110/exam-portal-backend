28e48a34df3422ad8c523fcf4363a497500c7ee5
KNL110 <asarikrunal9@gmail.com>
2025-07-12 16:23:22 +0530

    add global error

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { CROS_ORIGIN } from "./constants.js";
+import { globalError } from "./middlewares/globalerror.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended:true, limit:"16kb"}));
+app.use(globalError);
 
 
 export {app};
---
+++ src/middlewares/globalerror.js
@@ -0,0 +1,5 @@
+export const globalError = (err, req, res, next) => {
+    const status = err.statusCode || 500;
+    
+    return res.status(status).json({ success: false, message: err.message });
+} 

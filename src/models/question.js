de72e9cdca0dc71507ce262ecf77e592db6462c9
KNL110 <asarikrunal9@gmail.com>
2025-07-12 05:49:10 +0530

    add question model

---
+++ src/models/question.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+
+const questionSchema = new mongoose.Schema({
+    questionID: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    question: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    questionType: {
+        type: String,
+        enum: ['MCQ', 'NAT'],
+        required: true
+    },
+    options: {
+        type: [String],
+        validate: [
+            {
+                validator: function (val) {
+                    return val.length >= 2; // min options
+                },
+                message: 'There must be at least 2 options.'
+            },
+            {
+                validator: function (val) {
+                    return val.length <= 5; // max options
+                },
+                message: 'There cannot be more than 5 options.'
+            }
+        ]
+    },
+    image: {
+        type: String,    //url path or server path
+        trim: true,
+        default: ""
+    },
+    correctOption: {
+        type: Number     // For MCQ
+    },
+    correctValue: {
+        type: Number    // For NAT
+    },
+    marks: {
+        type: Number,
+        required: true,
+    },
+});
+
+export const question = mongoose.model("questions", questionSchema);

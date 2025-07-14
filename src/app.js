3aa7e32d9c9b5615f8f60bdd2aefe4ce388f5733
KNL110 <asarikrunal9@gmail.com>
2025-07-14 21:20:24 +0530

    add webpages for home,registration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import { CROS_ORIGIN } from "./constants.js";
 import { globalError } from "./middlewares/globalerror.js";
-import router from "./routes/candidateRoutes.js";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors({
     origin: CROS_ORIGIN,
@@ -13,6 +17,21 @@ app.use(cors({
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "home.html")); // Add pretty routes for your vanilla pages:
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html")); // Add pretty routes for your vanilla pages:
+});
+
+app.get("/registerCandidate", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "register-candidate.html"));
+});
+
+app.get("/registerProfessor", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "register-professor.html"));
+});
 
 //import routers
 import candidateRouter from "./routes/candidateRoutes.js";
@@ -22,5 +41,6 @@ import professorRouter from "./routes/professorRoutes.js";
 app.use("/api/v1/candidate", candidateRouter);
 app.use("/api/v1/professor", professorRouter);
 
+app.use(express.static(path.join(__dirname, "public")));  // Serve static files like CSS & JS
 app.use(globalError);
 export { app };
--- src/database/db.js
+++ src/database/db.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 import { DB_URI } from "../constants.js";
 
 const connectDB = async () => {
-    try {
-        console.log('Connecting to DB...');
-        const conn = await mongoose.connect(`${DB_URI}`, () => {
-            console.log(`Database Connected !!\nDB_HOST: ${conn.connection.host}`);
-            console.log("Connected to DB:", mongoose.connection.name);
-        });
+  try {
+    console.log('Connecting to DB...');
 
-    } catch (error) {
-        console.error("DATABASE CONNECTION FAILED!", error);
-        process.exit(1);
-    }
-}
+    const conn = await mongoose.connect(DB_URI);
+
+    console.log("Database Connected !!");
+    console.log(`DB_HOST: ${conn.connection.host}`);
+    console.log(`Connected to DB: ${conn.connection.name}`);
+
+  } catch (error) {
+    console.error("DATABASE CONNECTION FAILED!", error);
+    process.exit(1);
+  }
+};
+
 export default connectDB;

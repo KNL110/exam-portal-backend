87c0a9dd93a1668ac0a523da44bc539904daff74
KNL110 <asarikrunal9@gmail.com>
2025-07-15 17:31:21 +0530

    remove web pages from public

--- src/app.js
+++ src/app.js
@@ -9,21 +9,6 @@ app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "home.html")); // Add pretty routes for your vanilla pages:
-});
-
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "login.html")); // Add pretty routes for your vanilla pages:
-});
-
-app.get("/registerCandidate", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "register-candidate.html"));
-});
-
-app.get("/registerProfessor", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "register-professor.html"));
-});
 
 //import routers
 import candidateRouter from "./routes/candidateRoutes.js";
@@ -33,6 +18,5 @@ import professorRouter from "./routes/professorRoutes.js";
 app.use("/api/v1/candidate", candidateRouter);
 app.use("/api/v1/professor", professorRouter);
 
-app.use(express.static(path.join(__dirname, "public")));  // Serve static files like CSS & JS
 app.use(globalError);
 export { app };

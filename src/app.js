1aa84469c2722a08d64c3d013b32c883ac16b331
KNL110 <asarikrunal9@gmail.com>
2025-07-17 21:24:46 +0530

    add cookie parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from 'cookie-parser';
 import { CORS_ORIGIN } from "./constants.js";
 import { globalError } from "./middlewares/globalerror.js";
 
@@ -9,6 +10,7 @@ app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 
 //import routers
 import candidateRouter from "./routes/candidateRoutes.js";

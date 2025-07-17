058c2782e958e3734bfe03cef4c39a1cdc0b5601
KNL110 <asarikrunal9@gmail.com>
2025-07-17 21:21:54 +0530

    add refresh and access tokens in env

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,8 @@ export const PORT = process.env.PORT || 4000;
 export const DB_NAME = "examPortal";
 export const DB_URI = `${process.env.MONGODB_URI}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
+
+export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;

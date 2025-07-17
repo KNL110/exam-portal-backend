1a802305ca922847b98b926d738d6a5a7b599397
KNL110 <asarikrunal9@gmail.com>
2025-07-17 22:15:04 +0530

    add authorization middleware for both roles

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,44 @@
+import { ACCESS_TOKEN_SECRET } from "../constants.js";
+import { Candidate } from "../models/student.js";
+import { Professor } from "../models/professor.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+        const decodedtoken = jwt.verify(token, ACCESS_TOKEN_SECRET);
+
+        let user;
+
+        switch (decodedtoken.role) {
+            case 'candidate':
+                user = await Candidate.findById(decodedtoken?._id).select("-password -refreshtoken");
+                break;
+            case 'professor':
+                user = await Professor.findById(decodedtoken?._id).select("-password -refreshtoken");
+                break;
+            default:
+                throw new ApiError(401, 'Invalid access token');
+        }
+
+        if (!user) {
+            throw new ApiError(401, "User not found");
+        }
+
+        req.user = user;
+        req.user.role =decodedtoken.role;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access");
+    }
+
+})

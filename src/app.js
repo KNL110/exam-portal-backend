4a7ee684559442b6d5bb94add73eeb4f7c6b9f32
KNL110 <asarikrunal9@gmail.com>
2025-07-18 16:06:09 +0530

    add authroute in main app

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from 'cookie-parser';
 import { CORS_ORIGIN } from "./constants.js";
 import { globalError } from "./middlewares/globalerror.js";
+import authRoute from "./routes/authRoutes.js"
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors({
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use("/api/v1/auth", authRoute);
 
 //import routers
 import candidateRouter from "./routes/candidateRoutes.js";
